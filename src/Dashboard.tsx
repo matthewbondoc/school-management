651723759333465e6b439b70cdaba4afa9c6dfb5
mahttewbondoc <mahttewbondoc5@gmail.com>
2021-11-16 14:45:24 +0800

    update to dashboard

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -1,5 +1,12 @@
 
 import { Component } from "react";
+import { Navbar, NavbarBrand } from "reactstrap";
+import {
+    IoSchoolOutline,
+    IoMan,
+    IoSettings,
+    IoWomanOutline,
+} from "react-icons/io5";
 
 interface MyState {
 
@@ -9,10 +16,10 @@ export class Dashboard extends Component<{}, MyState> {
     render(){
         return(
             <div>
-                <Navbar color="maroon" light mb-2>
+                <Navbar color="dark" light mb-2>
                     <NavbarBrand className="text-white">
-                        <ToSchoolOutline className="font-size-xxl"/>
-                        <span className="font-size-l ml-3">Excellence University</span>
+                        <IoSchoolOutline className="font-size-xxl"/>
+                        <span className="font-size-l ml-3"> Excellence University</span>
                     </NavbarBrand>
                 </Navbar>
             </div>
